8eb10576f21bbf7b2f8ee6af593c1aa4873f7799
Corn3 <robineliasson93@gmail.com>
2021-08-10 08:39:08 +0200

    Added state and mutators.

--- src/store/index.js
+++ src/store/index.js
@@ -6,6 +6,30 @@ Vue.use(Vuex);
 export default new Vuex.Store({
     state: {
         triviaQuestions: [],
-        correctAnswers: []
+        answers: [],
+        user: {}
+    },
+    mutations: {
+        setTriviaQuestions: (state, payload) => {
+            state.triviaQuestions = payload;
+        },
+        addAnswer: (state, payload) => {
+            state.answers.push(payload);
+        },
+        emptyAnswers: (state) => {
+            state.answers = [];
+        }, 
+        setUser: (state, payload) => {
+            state.user = payload;
+        },
+        setUserPoints: (state, payload) => {
+            state.user.points = payload;
+        }
+    },
+    getters: {
+
+    },
+    actions: {
+        
     }
 })
