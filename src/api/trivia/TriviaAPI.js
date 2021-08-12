be0c664398d54c5dc4f0c75bbc36ca4f03a7613e
Corn3 <robineliasson93@gmail.com>
2021-08-12 12:25:18 +0200

    removed console.log

--- src/api/trivia/TriviaAPI.js
+++ src/api/trivia/TriviaAPI.js
@@ -15,7 +15,6 @@ export const TriviaAPI = {
     async fetchQuestions (preferences) {
         let API_URL = BASE_URL+(preferences[0])+((preferences[1] !== -1) ?
             ("&category=")+(preferences[1]) : "") + ("&difficulty=")+(preferences[2]);
-            console.log(API_URL);
         try{
             const response = await fetch(API_URL);
             const questions = await response.json();
